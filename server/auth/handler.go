cbd317eaa1fca56926e93b3ff88828038db7121d
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2025-07-21 01:41:41 +0900

    Server: Remove unused line

--- server/auth/handler.go
+++ server/auth/handler.go
@@ -2,7 +2,6 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/dirain1700/clearning/server/def"
@@ -14,7 +13,6 @@ func HandleAuthEntry(res http.ResponseWriter, req *http.Request) {
 	// try to get the user without re-authenticating
 	def.ModifyURLParams(req, "provider", "google")
 	_, err := gothic.CompleteUserAuth(res, req)
-	fmt.Printf("Attempting to complete user auth: %v\n", err)
 
 	if err == nil {
 		http.Redirect(res, req, "/info", http.StatusFound)
